49400be6a0b68ec8b278688187a2d2a2275fc93f
Kadek Satriadi <ananta.satriadi@gmail.com>
2024-05-17 10:48:45 +1000

    updates: added styles selection

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="corner">
+  <div class="corner info p-4">
     <h2 class="title">Bali Temples Map</h2>
     <p>
       The data is from
@@ -17,16 +17,57 @@
         ></small
       >
     </p>
+    <div class="legend mt-3">
+      <div class="legend-item">
+        <div class="legend-icon named"></div><div class="legend-label">Named point</div>
+      </div> 
+      <div class="legend-item">
+        <div class="legend-icon unnamed"></div><div class="legend-label">Unnamed point</div>
+      </div> 
+
+    </div>
     <hr />
-    <button @click="plotData" class="button is-success">Fetch and Plot</button>
+    <button @click="plotData" class="button is-success mb-4">Fetch and Plot</button>
+    <div class="buttons">
+      <button class="button" @click="changeTileLayer('Standard')">Standard</button>
+      <button class="button" @click="changeTileLayer('Hot')">Hot</button>
+      <button class="button" @click="changeTileLayer('OpenTopoMap')">OpenTopoMap</button>
+      <button class="button" @click="changeTileLayer('WorldImagery')">WorldImagery</button>
+      <!-- Add more buttons for other styles if needed -->
+    </div>
   </div>
-  <div id="map"></div>
+  <div id="map" ></div>
 </template>
 
 <style>
 .popup {
   max-height: 200px;
   overflow-y: scroll;
+}
+.legend{
+  display: flex;
+}
+.legend-item{
+  display: flex;
+  margin-right: 5px;
+  padding: 2px;
+}
+.unnamed{
+  background-color: #7570b3;
+}
+.named{
+  background-color: #d95f02;
+}
+.legend-icon{
+  width: 24px;
+  height: 24px;
+  border: 1px solid black;
+  display: block;
+  border-radius: 50%;
+}
+.legend-label{
+  display: block;
+  padding-left: 5px;
 }
 #map {
   width: 100%;
@@ -37,6 +78,10 @@
   top: 5%;
   z-index: 1000;
   right: 1%;
+}
+.info {
+  background-color: rgba(255, 255, 255, 0.6) !important;
+  border-radius: 5px;
 }
 </style>
 
@@ -55,6 +100,7 @@ export default {
     const map = ref(null);
     const csvUrl =
       "https://raw.githubusercontent.com/KadekSatriadi/OSM-Bali-temples/main/Bali_place_of_worship_all.csv";
+
 
     onMounted(async () => {
       // Initialize the map
@@ -103,18 +149,26 @@ export default {
       Papa.parse(csvDataString, {
         header: true,
         complete: (results: any) => {
-          console.log("Parsed data", results.data);
           results.data.forEach((row: any) => {
             const lat = parseFloat(row.Y);
             const lon = parseFloat(row.X);
+            let color;
+            // Check if the value is NA
+            if (row.name === "NA") {
+              // Color for NA values
+              color = "#7570b3";
+            } else {
+              // Color for non-NA values
+              color = "#d95f02";
+            }
             // console.log("lat", lat, "long", lon);
             if (!isNaN(lat) && !isNaN(lon)) {
               //L.marker([lat, lon]).addTo(map.value);
               L.circle([lat, lon], {
-                color: "red", // Circle border color
-                fillColor: "#f03", // Circle fill color
+                color: color, // Circle border color
+                fillColor: color, // Circle fill color
                 fillOpacity: 0.5, // Circle fill opacity
-                radius: 500, // Circle radius in meters
+                radius: 200, // Circle radius in meters
               })
                 .addTo(map.value)
                 .bindPopup(createPopupContent(row));
@@ -142,7 +196,37 @@ export default {
       }
     };
 
-    return { fetchData, plotData };
+    const changeTileLayer = (layerName:any) => {
+
+      // Set up the new tile layer based on the selected style
+      switch (layerName) {
+        case 'Standard':
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+          }).addTo(map.value);
+          break;
+        case 'Hot':
+          L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors, Tiles style by <a href="https://www.hotosm.org/">Humanitarian OpenStreetMap Team</a>'
+          }).addTo(map.value);
+          break;
+        case 'OpenTopoMap':
+          L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenTopoMap'
+          }).addTo(map.value);
+          break;
+        case 'WorldImagery':
+          L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: '&copy; Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'
+          }).addTo(map.value);
+          break;
+        // Add more cases for other styles if needed
+        default:
+          break;
+      }
+    };
+
+    return { fetchData, plotData, changeTileLayer };
   },
 };
 </script>
